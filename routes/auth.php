bbea8727adc01b3157bf77efcc0eda6d548b0260
Seah <seahdokki@gmail.com>
2024-08-17 16:08:30 +0200

    fix register route

--- routes/auth.php
+++ routes/auth.php
@@ -16,7 +16,7 @@ Route::get('discord/callback', [\App\Http\Controllers\DiscordController::class, 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'index'])->name('register.create');
     //Route::get('register/{token}', [RegisteredUserController::class, 'create'])->name('register.create');
-    //Route::post('register/{token}', [RegisteredUserController::class, 'store']);
+    Route::post('register', [RegisteredUserController::class, 'store']);
     Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
